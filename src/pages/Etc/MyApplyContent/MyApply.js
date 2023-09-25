252ae79cae70aa03c413c6e7c5eeeefe60af714d
Suhsowon <rosec77@sookmyung.ac.kr>
2023-09-25 16:48:40 +0900

    MyApply 백업 및 FlatList 추가

--- src/pages/Etc/MyApplyContent/MyApply.js
+++ src/pages/Etc/MyApplyContent/MyApply.js
@@ -1,56 +1,81 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+import { Card, Text } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Axios from 'axios';
+import Loading from '../../../components/Loading';
 
 const MyApply = () => {
     const navigation = useNavigation();
 
     const [applyList, setApplyList] = React.useState([]);
 
+    const [loading, setLoading] = React.useState(true);
+
     React.useEffect(() => {
         const load = async () => {
-          try {
-            token = await AsyncStorage.getItem('token');
-    
-            const variables = {
-            }
-    
-            Axios.post('http://3.37.125.95:3000/applyList', variables, {headers:{authorization: token}})
-              .then(response => {
-                if (response.data.success) {
-                    setApplyList(response.data.data);
-                } else {
-                  alert('로딩 실패');
+            try {
+                setLoading(true);
+                token = await AsyncStorage.getItem('token');
+
+                const variables = {
                 }
-              })
-          } catch (error) {
-            console.log(error);
-          }
+                await Axios.post('http://3.37.125.95:3000/users/applyList', variables, { headers: { authorization: token } })
+                    .then(response => {
+                        if (response.data.success) {
+                            console.log('응모 내역 로딩 성공');
+                            console.log(response.data.data);
+                            setApplyList(response.data.data);
+                        } else {
+                            console.log('응모 내역 로딩 실패');
+                        }
+                    })
+            } catch (error) {
+                console.log(error);
+            }
+            setLoading(false);
         }
         load();
-      }, []);
+    }, []);
 
-    let itemView = (applyList.length > 0 ? ticketList.map((item, index) => {
+    // let applyView = (applyList.length > 0 ? applyList.map((item, index) => {
+    //     return (
+    //         <Card mode='outlined' style={styles.box} key={index} onPress={() => navigation.navigate('ShowApplyResult', { item: item })}>
+    //             <Card.Content style={{ margin: 10 }}>
+    //                 <Text variant="titleLarge">{item.showname}</Text>
+    //                 {/* <Text />
+    //                 <View style={{ flexDirection: 'row' }}>
+    //                     <Text variant="bodyLarge">{item.showtime}</Text>
+    //                     <Text variant="bodyLarge" style={{ flex: 1, textAlign: 'right' }}>{item.ticketPrice} ETH</Text>
+    //                 </View> */}
+    //             </Card.Content>
+    //         </Card>
+    //     );
+    // }) : <View style={styles.container}><Text>응모 내역이 없습니다.</Text></View>)
+
+    let applyView = (applyList.length > 0 ? <FlatList data={applyList} renderItem={(itemData) => {
         return (
-            <Card style={styles.box} key={index} onPress={() => {}}>
+            <Card mode='outlined' style={styles.box} onPress={() => navigation.navigate('ShowApplyResult', { item: itemData.item })}>
                 <Card.Content style={{ margin: 10 }}>
-                    <Text variant="titleLarge">{item.showname}</Text>
-                    <Text />
+                    <Text variant="titleLarge">{itemData.item.showname}</Text>
+                    {/* <Text />
                     <View style={{ flexDirection: 'row' }}>
-                        <Text variant="bodyLarge">{item.showtime}</Text>
-                        <Text variant="bodyLarge" style={{ flex: 1, textAlign: 'right' }}>{item.ticketPrice}</Text>
-                    </View>
+                        <Text variant="bodyLarge">{itemData.item.showtime}</Text>
+                        <Text variant="bodyLarge" style={{ flex: 1, textAlign: 'right' }}>{itemData.item.ticketPrice} ETH</Text>
+                    </View> */}
                 </Card.Content>
             </Card>
         );
-    }) : <View><Text>응모 내역이 없습니다.</Text></View>)
+    }} />
+        : <View style={styles.container}><Text>응모 내역이 없습니다.</Text></View>)
 
     return (
-        <ScrollView contentContainerStyle={{alignItems:'center', justifyContent:'center'}}>
-            {itemView}
-        </ScrollView>
+        <View style={{ flex: 1, marginHorizontal: 25 }}>
+            {loading ? <Loading /> : <View style={{ flex: 1 }}>
+                {applyView}
+            </View>}
+        </View>
     );
 }
 
@@ -62,9 +87,7 @@ const styles = StyleSheet.create({
     },
     box: {
         alignContent: 'center',
-        margin: 20,
-        marginTop: 10,
-        marginBottom: 10
+        marginVertical: 15
     }
 });
 
