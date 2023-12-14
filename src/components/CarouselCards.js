4d569c26be29e14343dd2a5f30393bd97e1884be
Suhsowon <rosec77@sookmyung.ac.kr>
2023-12-14 20:06:47 +0900

    CarouselCards 더미데이터 제거 및 넘겨받은 데이터 출력 양식 수정

--- src/components/CarouselCards.js
+++ src/components/CarouselCards.js
@@ -2,51 +2,15 @@ import React from 'react'
 import { View, TouchableOpacity, StyleSheet, Image, Text, Dimensions } from "react-native"
 import Carousel, { Pagination } from 'react-native-snap-carousel'
 import { useNavigation } from '@react-navigation/native';
-import Axios from 'axios';
 
 const SLIDER_WIDTH = Dimensions.get('window').width;
 const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
 
-const CarouselCards = () => {
+const CarouselCards = ({ data }) => {
   const navigation = useNavigation();
 
   const [index, setIndex] = React.useState(0);
   const isCarousel = React.useRef(null);
-
-  const [data, setData] = React.useState([
-    {
-      title: "크러쉬 팬미팅",
-      place: "-돔",
-      time: "5/03 14:00",
-      seats: "A1",
-      imgUrl: "https://picsum.photos/id/11/200/300",
-    },
-    {
-      title: "김연경 콘서트",
-      place: "-돔",
-      time: "5/22 18:00",
-      seats: "D12",
-      imgUrl: "https://picsum.photos/id/10/200/300",
-    },
-    {
-      title: "양지은 콘서트",
-      place: "-돔",
-      time: "5/02 12:00",
-      seats: "C5",
-      imgUrl: "https://picsum.photos/id/12/200/300",
-    },
-  ]);
-
-  // React.useEffect(() => {
-  //   Axios.post('http://3.37.125.95:3000/main', variable)
-  //     .then(response => {
-  //       if (response.data.success) {// response.data = Object 형태
-  //         setData(response.data.videoDetail);
-  //       } else {
-  //         alert('홈페이지 로딩 실패');
-  //       }
-  //     })
-  // }, []);// 여기 useisfocused 넣으면 뒤로 가는걸로 인식될까?
 
   return (
     <View>
@@ -61,12 +25,23 @@ const CarouselCards = () => {
                 <TouchableOpacity onPress={() => navigation.navigate('ShowInfo', { item: item })}>
                   <View style={styles.container} key={index}>
                     <Image
-                      source={{ uri: item.imgUrl }}
+                      source={{ uri: `data:image/jpg;base64,${item.imgEncode}` }}
                       style={styles.image}
                     />
-                    <Text style={styles.header}>{item.title}</Text>
+                    <Text numberOfLines={1} style={styles.header}>{item.showname}</Text>
                     <Text />
-                    <Text style={styles.body}>{item.time}</Text>
+
+                    <View style={{ marginLeft: 20, marginRight: 20, flexDirection: 'row', justifyContent: 'space-between' }}>
+                      <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                        {item.reservedSeat.map((seat, i) => {
+                          return (
+                            <Text key={i} style={styles.body}>A{seat.seatid}  </Text>
+                          );
+                        })}
+                      </View>
+                      <Text style={styles.body}>{item.showdate}</Text>
+                    </View>
+
                   </View>
                 </TouchableOpacity>
               )
@@ -114,21 +89,22 @@ const styles = StyleSheet.create({
     marginTop: 70,
   },
   image: {
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
     width: ITEM_WIDTH,
     height: 300,
   },
   header: {
     color: "#222",
-    fontSize: 28,
+    fontSize: 22,
     fontWeight: "bold",
     paddingLeft: 20,
+    paddingRight: 20,
     paddingTop: 20
   },
   body: {
     color: "#222",
-    fontSize: 18,
-    paddingLeft: 25,
-    paddingRight: 20
+    fontSize: 16,
   }
 })
 
